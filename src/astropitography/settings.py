41d98766821e54230fdc84f5e6ea91371716b161
Adam <adamlukebaskerville@gmail.com>
2024-02-18 13:16:32 +0000

    Disabled CI content as runners not support arm32

--- src/astropitography/settings.py
+++ src/astropitography/settings.py
@@ -1,11 +1,11 @@
 import os
+from typing import Literal
 import PySimpleGUI as sg
 
 # grab the resolution of the screen the program is being run on
 SCREEN_WIDTH, SCREEN_HEIGHT = sg.Window.get_screen_size()
 
+IMAGE_SAVE_FOLDER: str = "{}/images".format(os.getcwd())
+VIDEO_SAVE_FOLDER: str = "{}/videos".format(os.getcwd())
 
-IMAGE_SAVE_FOLDER = "{}/images".format(os.getcwd())
-VIDEO_SAVE_FOLDER = "{}/videos".format(os.getcwd())
-
-RESOLUTION_OPTIONS = ["640 x 480", "1280 x 720", "1920 x 1080", "2560 x 1440"]
+RESOLUTION_OPTIONS: Literal["640 x 480", "1280 x 720", "1920 x 1080", "2560 x 1440"]
